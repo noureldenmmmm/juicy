8f4f4378105c194fae46ae8b164f9bf64f9a5359
noureldenmmmm <111704021+noureldenmmmm@users.noreply.github.com>
2026-09-01 17:04:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const Product = require("./schema")
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}));
+app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
   Product.find()
@@ -21,9 +24,13 @@ app.get('/user/add.ejs',(req,res) => {
   res.render('user/add')
 })
 
-app.post('/user/add.ejs',(req,res) => {
-  const newProduct = new Product(req.body)
-  
+app.post('/user/add.ejs',upload.single('image'),(req,res) => {
+  const newProduct = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    image: req.file.path
+  })
   newProduct.save()
   .then(() => {
     res.redirect('/')
